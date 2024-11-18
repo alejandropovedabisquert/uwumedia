5823988f0bb7862e0c17bc171acd536df5c50ab2
alejandropovedabisquert <alejandropovedabisquert@gmail.com>
2024-11-18 17:35:15 +0100

    Refactorizacion seasonal animes

--- app/anime/seasonal/page.tsx
+++ app/anime/seasonal/page.tsx
@@ -1,10 +1,17 @@
-import SeasonalSections from "@/app/ui/anime/seasonal/seasonal-sections";
-
+import { fetchSeasonalAnime } from "@/app/lib/data";
+import { CardsWrapper } from "@/app/ui/anime/seasonal/CardsWrapper";
 export default async function Page() {
-    
+    const seasonalList = await fetchSeasonalAnime();
     return (
         <main className="max-w-screen-2xl m-auto">
-            <SeasonalSections/>
+                <CardsWrapper media_type="tv" data={seasonalList.data}/>
+                <CardsWrapper media_type="ona" data={seasonalList.data}/>
+                <CardsWrapper media_type="ova" data={seasonalList.data}/>
+                <CardsWrapper media_type="movie" data={seasonalList.data}/>
+                <CardsWrapper media_type="special" data={seasonalList.data}/>
+                <CardsWrapper media_type="tv_special" data={seasonalList.data}/>
+                <CardsWrapper media_type="pv" data={seasonalList.data}/>
+                <CardsWrapper media_type="music" data={seasonalList.data}/>
         </main>
     );
 }
