211fb5230fc0ed4649a34ddeba3b5dbd049703da
alejandropovedabisquert <alejandropovedabisquert@gmail.com>
2024-11-21 15:58:30 +0100

    Prueba simulacion retardo seasonal anime

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,20 +1,19 @@
 export async function fetchSeasonalAnime() {
-    console.log('Fetching seasonal anime data...')
+    
     try {
+        console.log("Llamando a seasonal anime");
+        
+        await new Promise(resolve => setTimeout(resolve, 3000)); // Retraso de 2 segundos
+
+        console.log("Ha cargado seasonal anime en 3 segundos");
+        
         const response = await fetch("https://api.myanimelist.net/v2/anime/season/2024/fall?limit=500&nsfw=true&fields=id,title,main_picture,start_date,mean,nsfw,media_type,genres,num_episodes,average_episode_duration", {
             method: 'GET', // Method is optional because GET is the default
             headers: {
                 'X-MAL-CLIENT-ID': process.env.X_MAL_CLIENT_ID!
             }
         });
-        // Usa setTimeout para retrasar la lectura y conversión del JSON;
-        const data = await new Promise((resolve, reject) => {
-            setTimeout(() => {
-                response.json().then(resolve).catch(reject);
-            }, 3000); // Retrasa por 2000 ms = 2 segundos
-        });
-        console.log("Pasaron 3 segundos...");
-
+        const data = await response.json()
         
         return data;
     } catch (error) {
