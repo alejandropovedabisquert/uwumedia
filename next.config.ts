d13821a47f9027da72892da34c6ee6d39a9b7293
alejandropovedabisquert <alejandropovedabisquert@gmail.com>
2024-11-21 16:39:01 +0100

    Prueba simulacion retardo seasonal anime

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    images: {
+  images: {
     domains: ['cdn.myanimelist.net'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*{/}?',
+        headers: [
+          {
+            key: 'X-Accel-Buffering',
+            value: 'no',
+          },
+        ],
+      },
+    ]
   },
 };
 
