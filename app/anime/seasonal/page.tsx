fc762caa862262d91acc75248b9fcb0e82c4ba3f
alejandropovedabisquert <alejandropovedabisquert@gmail.com>
2024-11-21 16:55:35 +0100

    Cambio loading method

--- app/anime/seasonal/page.tsx
+++ app/anime/seasonal/page.tsx
@@ -1,11 +1,7 @@
 import { CardsWrapper } from "@/app/ui/anime/seasonal/CardsWrapper";
-import { CardsSkeleton } from "@/app/ui/skeletons";
-import { Suspense } from "react";
 export default function Page() {
     
     return (
-        <Suspense fallback={<CardsSkeleton/>}>
-            <CardsWrapper/>
-        </Suspense>
+        <CardsWrapper/>
     );
 }
